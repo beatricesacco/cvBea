667432f0aec96c39662ec4365d07b903ac3409e3
Beatrice Sacco <saccobeatrice98@gmail.com>
2025-01-15 18:49:54 +0100

    Aggiunta app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("Ok");
+});
+
+app.listen(port, console.log("Server in ascolto"));
