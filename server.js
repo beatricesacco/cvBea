c8cc96eb724cc1907427ed2a265d54df02a7d99c
Beatrice Sacco <saccobeatrice98@gmail.com>
2025-01-17 16:28:08 +0100

    Update and rename app.js to server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(helmet());
-app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Ok");
