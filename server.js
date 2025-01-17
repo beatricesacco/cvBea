c55374efbc3f9224b0786323f114466362686c20
Beatrice Sacco <saccobeatrice98@gmail.com>
2025-01-17 18:53:38 +0100

    modifiche ai file richieste.http e server.js

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("Richiesta ricevuta alla rotta radice");
+  res.send("<h1>Benvenuto nel mio sito curriculum!</h1>");
+});
+
+app.get("/profilo", (req, res) => {
+  res.send("<h1>Questa è la pagina del mio profilo</h1>");
+});
+
+app.get("/competenze", (req, res) => {
+  res.send("<h1>Questa è la pagina delle mie competenze</h1>");
+});
+
+app.get("/contatti", (req, res) => {
+  res.send("<h1>Questa è la pagina dei miei contatti</h1>");
 });
 
 app.listen(3000, () => console.log("Web server in ascolto sulla porta 3000"));
